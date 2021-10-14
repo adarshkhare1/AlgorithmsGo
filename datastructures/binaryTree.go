a179d16e93a97a1ac762e5937d87112c3de63bc2
adarshkhare1 <adarshkhare@hotmail.com>
2021-10-13 23:46:14 -0700

    Implemented Tree.Insert to build tree
    Now tree InOrder is correctly print nodes in order.

--- datastructures/binaryTree.go
+++ datastructures/binaryTree.go
@@ -10,15 +10,107 @@ type TreeNode struct {
 	Value int
 	Left  *TreeNode
 	Right *TreeNode
+	Parent *TreeNode
 }
 
 type BinaryTree struct {
-	root *TreeNode
+	Root *TreeNode
 }
 
-func NewBinaryTree(root *TreeNode) *BinaryTree {
-	return &BinaryTree{root: root}
+func NewBinaryTree(rootValue int) *BinaryTree {
+	root := _newTreeNode(rootValue)
+	return &BinaryTree{Root: root}
 }
+
+func (t *BinaryTree) Depth() uint {
+	return _calculateDepth(t.Root, 0)
+}
+
+func (t *BinaryTree)Insert(k int) *TreeNode{
+	newNode := _newTreeNode(k)
+	x := t.Root
+	var y *TreeNode
+	for x != nil {
+		y = x
+		if newNode.Value < x.Value {
+			x = x.Left
+		} else {
+			x = x.Right
+		}
+	}
+	newNode.Parent = y
+	if y == nil{ // Empty tree
+		t.Root = newNode
+	} else if newNode.Value < y.Value {
+		y.Left = newNode
+	} else {
+		y.Right = newNode
+	}
+	return newNode
+}
+
+func (t *BinaryTree)Inorder() string{
+	return _inorder(t.Root, "")
+}
+
+func (t *BinaryTree)Preorder() string{
+	return _preorder(t.Root, "")
+}
+
+func (t *BinaryTree)Postorder() string{
+	return _postOrder(t.Root, "")
+}
+
+func _newTreeNode(val int) *TreeNode {
+	n := &TreeNode{Value:val,Left: nil, Right:nil, Parent:nil}
+	return n
+}
+
+// Inorder traversal print left node, Root, right node order
+func _inorder(n *TreeNode, result string) string {
+	if n == nil {
+		return result
+	}
+	if n.Left != nil {
+		result = _inorder(n.Left, result)
+	}
+	result += " " + strconv.Itoa(n.Value)
+	if n.Right != nil {
+		result = _inorder(n.Right, result)
+	}
+	return result
+}
+
+// PostOrder traversal print left node, right node, Root order
+func _postOrder( n *TreeNode, result string) string {
+	if n == nil {
+		return result
+	}
+	if n.Left != nil {
+		result = _postOrder(n.Left, result)
+	}
+	if n.Right != nil {
+		result = _postOrder(n.Right, result)
+	}
+	result += " " + strconv.Itoa(n.Value)
+	return result
+}
+
+// PreOrder traversal prints Root, left node, right node order
+func _preorder(n *TreeNode, result string) string {
+	if n == nil {
+		return result
+	}
+	result += " " + strconv.Itoa(n.Value)
+	if n.Left != nil {
+		result = _preorder(n.Left, result)
+	}
+	if n.Right != nil {
+		result = _preorder(n.Right, result)
+	}
+	return result
+}
+
 
 // helper function for t.depth
 func _calculateDepth(n *TreeNode, depth uint) uint {
@@ -28,7 +120,6 @@ func _calculateDepth(n *TreeNode, depth uint) uint {
 	return _max(_calculateDepth(n.Left, depth+1), _calculateDepth(n.Right, depth+1))
 }
 
-
 func _max(a, b uint) uint {
 	if a > b {
 		return a
@@ -36,58 +127,4 @@ func _max(a, b uint) uint {
 	return b
 }
 
-func NewTreeNode(val int) *TreeNode {
-	n := &TreeNode{val, nil, nil}
-	return n
-}
-
-// Inorder traversal print left node, root, right node order
-func Inorder(n *TreeNode, result string) string {
-	if n == nil {
-		return result
-	}
-	if n.Left != nil {
-		result = Inorder(n.Left, result)
-	}
-	result += " " + strconv.Itoa(n.Value)
-	if n.Right != nil {
-		result = Inorder(n.Right, result)
-	}
-	return result
-}
-
-// PostOrder traversal print left node, right node, root order
-func PostOrder(n *TreeNode, result string) string {
-	if n == nil {
-		return result
-	}
-	if n.Left != nil {
-		result = PostOrder(n.Left, result)
-	}
-	if n.Right != nil {
-		result = PostOrder(n.Right, result)
-	}
-	result += " " + strconv.Itoa(n.Value)
-	return result
-}
-
-// PreOrder traversal prints root, left node, right node order
-func PreOrder(n *TreeNode, result string) string {
-	if n == nil {
-		return result
-	}
-	result += " " + strconv.Itoa(n.Value)
-	if n.Left != nil {
-		result = PreOrder(n.Left, result)
-	}
-	if n.Right != nil {
-		result = PreOrder(n.Right, result)
-	}
-	return result
-}
-
-
-func (t *BinaryTree) Depth() uint {
-	return _calculateDepth(t.root, 0)
-}
 
--- datastructures/binaryTree_test.go
+++ datastructures/binaryTree_test.go
@@ -7,64 +7,49 @@ import (
 
 
 
-func TestTreeInOrder(t *testing.T) {
+func TestTreeTraversal(t *testing.T) {
 	tree := getSampleTestTree()
-	testHelper.VerifyUintsAreEqual(t, 4, tree.Depth())
-	testHelper.VerifyStringsAreEqual(t, " 3 1 4 0 5 2 6 10", Inorder(tree.root, ""))
-}
-
-func TestTreePreOrder(t *testing.T) {
-	tree := getSampleTestTree()
-	testHelper.VerifyUintsAreEqual(t, 4, tree.Depth())
-	testHelper.VerifyStringsAreEqual(t, " 0 1 3 4 2 5 6 10", PreOrder(tree.root, ""))
-}
-
-func TestTreePostOrder(t *testing.T) {
-	tree := getSampleTestTree()
-	testHelper.VerifyUintsAreEqual(t, 4, tree.Depth())
-	testHelper.VerifyStringsAreEqual(t, " 3 4 1 5 10 6 2 0", PostOrder(tree.root, ""))
+	testHelper.VerifyUintsAreEqual(t, 6, tree.Depth())
+	testHelper.VerifyStringsAreEqual(t, " 0 1 2 3 4 5 6 10", tree.Inorder())
+	testHelper.VerifyStringsAreEqual(t, " 0 1 4 3 2 10 6 5", tree.Preorder())
+	testHelper.VerifyStringsAreEqual(t, " 2 3 5 6 10 4 1 0", tree.Postorder())
 }
 
 func TestTreeInOrderDepth1(t *testing.T) {
 	tree := getSampleTreeWithDepth1()
-	testHelper.VerifyUintsAreEqual(t, 2, tree.Depth())
-	testHelper.VerifyStringsAreEqual(t, " 1 0 2", Inorder(tree.root, ""))
+	testHelper.VerifyUintsAreEqual(t, 3, tree.Depth())
+	testHelper.VerifyStringsAreEqual(t, " 0 1 2", tree.Inorder())
 }
 
 func TestTreePreOrderDepth1(t *testing.T) {
 	tree := getSampleTreeWithDepth1()
-	testHelper.VerifyUintsAreEqual(t, 2, tree.Depth())
-	testHelper.VerifyStringsAreEqual(t, " 0 1 2", PreOrder(tree.root, ""))
+	testHelper.VerifyUintsAreEqual(t, 3, tree.Depth())
+	testHelper.VerifyStringsAreEqual(t, " 0 1 2", tree.Preorder())
 }
 
 func TestTreePostOrderDepth1(t *testing.T) {
 	tree := getSampleTreeWithDepth1()
-	testHelper.VerifyUintsAreEqual(t, 2, tree.Depth())
-	testHelper.VerifyStringsAreEqual(t, " 1 2 0", PostOrder(tree.root, ""))
+	testHelper.VerifyUintsAreEqual(t, 3, tree.Depth())
+	testHelper.VerifyStringsAreEqual(t, " 2 1 0", tree.Postorder())
 }
 
 
 func getSampleTestTree() *BinaryTree {
-	tree := NewBinaryTree(NewTreeNode(0))
-	tree.root.Left = NewTreeNode(1)
-	tree.root.Right = NewTreeNode(2)
-	tree.root.Left.Left = NewTreeNode(3)
-	tree.root.Left.Right = NewTreeNode(4)
-	tree.root.Right.Left = NewTreeNode(5)
-	tree.root.Right.Right = NewTreeNode(6)
-	tree.root.Right.Right.Right = NewTreeNode(10)
+	tree := NewBinaryTree(0)
+	tree.Insert(1)
+	tree.Insert(4)
+	tree.Insert(3)
+	tree.Insert(10)
+	tree.Insert(6)
+	tree.Insert(5)
+	tree.Insert(2)
+
 	return tree
 }
 
 func getSampleTreeWithDepth1() *BinaryTree {
-	tree := NewBinaryTree(NewTreeNode(0))
-	tree.root.Left = NewTreeNode(1)
-	tree.root.Right = NewTreeNode(2)
+	tree := NewBinaryTree(0)
+	tree.Insert(1)
+	tree.Insert(2)
 	return tree
 }
-
-func verifyBinaryTree(t *testing.T, tree *BinaryTree, expected string){
-	/* read the list until head is not nil */
-	testHelper.VerifyStringsAreEqual(t, expected, Inorder(tree.root, ""))
-}
-
